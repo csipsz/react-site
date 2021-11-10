1a2c38a8f0d6f7198c1f191d74f2e5de70c79672
Luca <lucakanyo@protonmail.com>
2021-11-10 09:53:30 -0500

    added routes and create form

--- src/usefetch.js
+++ src/usefetch.js
@@ -6,7 +6,9 @@ const useFetch = (url) => {
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        fetch(url)
+        const abortCont = new AbortController();
+        setTimeout(() => {
+        fetch(url, { signal: abortCont.signal })
         .then(res => {
             if (!res.ok){
                 throw Error('Sorryka, could not fetch the data for that resource')
@@ -19,9 +21,15 @@ const useFetch = (url) => {
             setError(null);
         })
         .catch(err => {
-            setIsPending(false);
-            setError(err.message);
+            if (err.name === "AbortError"){
+                console.log('fetch aborted')
+            } else {
+                setIsPending(false);
+                setError(err.message);
+            }
         })
+    }, 1000);
+        return () => abortCont.abort()
     }, [url])
 
     return {data, isPending, error}
