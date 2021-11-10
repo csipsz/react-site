66ed5352b0adf34f14706f60c1ed6e24c9233650
Luca <lucakanyo@protonmail.com>
2021-11-10 10:06:40 -0500

    added delete blog

--- src/Create.js
+++ src/Create.js
@@ -1,20 +1,27 @@
 import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 const Create = () => {
 
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
-    const [author, setAuthor] = useState('Brad')
+    const [author, setAuthor] = useState('Brad');
+    const [isPending, setIsPending] = useState(false);
+    const history = useHistory();
 
     const handleSubmit = (e) => {
         e.preventDefault()
         const blog = { title, body, author};
+        setIsPending(true)
         fetch('http://localhost:8000/blogs', {
             method: 'POST', 
             headers: {"Content-Type": 'application/json'}, 
             body: JSON.stringify(blog)
         }).then(() => {
             console.log("New blog added")
+            setIsPending(false)
+            // history.go(-1)
+            history.push('/')
         })
     }
     
@@ -31,7 +38,8 @@ const Create = () => {
                     <option value="Brad">Brad</option>
                     <option value="Brandon">Brandon</option>
                 </select>
-                <button>Add blog</button>
+                {!isPending && <button>Add blog</button>}
+                {isPending && <button disabled>Adding blog...</button>}
             </form>
         </div>
      );
